ad0ab99311b5e60b88758e000bc758698ef922eb
Wayne Gretton <51050541+ThriveOnDesign@users.noreply.github.com>
2021-03-10 12:19:47 +0800

    first commit

---
+++ assests/script/thriveProductSlider.js
@@ -0,0 +1,46 @@
+// slider for wooCommerce products
+
+
+//create a variable that contains the swiper DOM element this is done so that I can add event listeners to it later  
+const sliderContainer = document.querySelector('.thrive-product-swiper');
+
+
+var thriveProductSwiper = new Swiper('.thrive-product-swiper', {
+  
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: 1,
+    centeredSlides: true,
+    speed: 2000,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: true,
+    },
+    
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  }
+}
+);
+
+// event listener added to the slider that will stop the slider from playing once mouse moves over it.
+sliderContainer.addEventListener('mouseover', function() {
+  thriveProductSwiper.autoplay.stop();
+})
+
+// event listener added to the slider that will re-start the slider once the mouse is no longer hovering over it.
+sliderContainer.addEventListener('mouseout', function() {
+  thriveProductSwiper.autoplay.start();
+})
---
+++ thrive-slesh-product-shortcode.php
@@ -0,0 +1,135 @@
+<?php
+   /*
+   Plugin Name: Thrive Woo Product Shortcode
+   Plugin URI: http://thriveondesign.dev
+   description: a custom plugin created by ThriveOnDesign for BeautySlesh that creates a shortcode on the home page to display woocommerce products in a custom way
+   Version: 1.0
+   Author: Wayne @ ThriveOnDesign
+   Author URI: http://thriveondesign.dev
+   License: GPL2
+   */
+
+   /**
+    *
+    * The slider/carousel in this plugin makes use of the swiper js library that is pacakged with elementor
+    *
+    */
+
+  /**
+   * This function is used to create a shortcode that displays the products in a slider on the homepage
+   */
+  function thriveFeaturedProducts(){
+    $query_args = [
+      'post_type' => 'product',
+    ];
+    
+    $result = new WP_Query($query_args);
+    // $product = wc_get_product(the_ID());
+    ?>
+  
+
+  <div class="thrive-product-section-container">
+    <div class="thrive-product-swiper swiper-container">
+      <div class="thrive-grid-container swiper-wrapper">
+        
+        <?php if ($result->have_posts()) {
+          while ($result->have_posts()) : $result->the_post();
+          ?>
+          <div class="thrive-grid-card swiper-slide">
+            <div class="thrive-image-container">
+              <?php the_post_thumbnail( ); ?>
+              <div class="thrive-product-overlay">
+                <?php woocommerce_simple_add_to_cart(); ?>
+              </div>
+            </div>
+            <div class="thrive-card-detail">
+              <p class="tod-product-description"><?php the_title(); ?></p>          
+              <?php $product = wc_get_product( get_the_ID() ); /* get the WC_Product Object */ ?>
+              <p class="tod-price"><?php echo $product->get_price_html(); ?></p>
+            </div>
+          </div>
+          <?php
+      endwhile;
+    }
+    ?>
+      </div>
+      <div class="thrive-slider-navigation swiper-button-prev"></div>
+      <div class="thrive-slider-navigation swiper-button-next"></div>
+    </div>
+  </div>
+  
+  
+
+  <?php
+  }
+
+  /**
+   * This function controls the display of the products on the products page
+   */
+  function thriveProductPage(){
+    $query_args = [
+      'post_type' => 'product',
+    ];
+    
+    $result = new WP_Query($query_args);
+    
+    ?>
+  
+    <div class="thrive-products-container">
+      
+      <?php if ($result->have_posts()) {
+        while ($result->have_posts()) : $result->the_post();
+        ?>
+        <div class="thrive-grid-card">
+          <div class="thrive-image-container">
+            <?php the_post_thumbnail( ); ?>
+            <div class="thrive-product-overlay">
+              <?php woocommerce_simple_add_to_cart(); ?>
+            </div>
+          </div>
+          <div class="thrive-card-detail">
+            <p class="tod-product-description"><?php the_title(); ?></p>          
+            <?php $product = wc_get_product( get_the_ID() ); /* get the WC_Product Object */ ?>
+            <p class="tod-price"><?php echo $product->get_price_html(); ?></p>
+          </div>
+        </div>
+        <?php
+    endwhile;
+  }
+    ?>
+  </div>
+     
+  
+  
+  
+
+  <?php
+  }
+
+
+  /**
+   * load the javascript file that controls the carousel for this plug in it is dependent on JQuery as well as swiper js that comes with Elementor
+   */ 
+
+  function thrive_plugin_scripts() {
+    wp_enqueue_script ('thriveProductSlider', '/wp-content/plugins/thrive-slesh-product-shortcode/assests/script/thriveProductSlider.js', ['jquery', 'swiper'], time(), true);
+  } 
+  
+  add_action( 'wp_enqueue_scripts', 'thrive_plugin_scripts');
+
+  // load the styles for the plugin
+
+  function enqueue_thrive_product_style() {
+    wp_enqueue_style('thriveProductStyles', '/wp-content/plugins/thrive-slesh-product-shortcode/assests/css/thriveProductStyles.css', '', time());
+  }
+  
+  add_action('wp_enqueue_scripts', 'enqueue_thrive_product_style', 11);
+
+
+  /**
+   * This creates the shortcode that can be inserted anywhere in WP
+   */
+  add_shortcode('thrive-featured-products', 'thriveFeaturedProducts');
+  add_shortcode('thrive-products-page', 'thriveProductPage');
+
+?>
