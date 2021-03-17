ca57b97f0cf815df65d9219d447350abd91f96d6
Wayne Gretton <51050541+ThriveOnDesign@users.noreply.github.com>
2021-03-17 16:25:59 +0800

    plus button animation and close

--- assests/script/thriveProductCustScript.js
+++ assests/script/thriveProductCustScript.js
@@ -3,11 +3,12 @@ const modal = document.querySelectorAll('.thrive-product-overlay');
 
 function handlePlusClick(e) {
   // overlay is a the previous sibling of the "button"
+  const currentPlus = e.currentTarget;
   const currentCard = e.currentTarget.previousElementSibling;
  
   // toggle "open" class for the over lay
   currentCard.classList.contains('open') ? currentCard.classList.remove('open') : currentCard.classList.add('open');
-  
+  currentPlus.classList.contains('open') ? currentPlus.classList.remove('open') : currentPlus.classList.add('open');
   
 }
 
