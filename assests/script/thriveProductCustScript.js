b8ebee38c474899e79264d27bfe18056b52e99ba
Wayne Gretton <51050541+ThriveOnDesign@users.noreply.github.com>
2021-03-26 22:31:57 +0800

    playing around with cart

--- assests/script/thriveProductCustScript.js
+++ assests/script/thriveProductCustScript.js
@@ -5,17 +5,44 @@ function handlePlusClick(e) {
   // overlay is a the previous sibling of the "button"
   const currentPlus = e.currentTarget;
   const currentCard = e.currentTarget.previousElementSibling;
- 
+  
   // toggle "open" class for the over lay
   currentCard.classList.contains('open') ? currentCard.classList.remove('open') : currentCard.classList.add('open');
   currentPlus.classList.contains('open') ? currentPlus.classList.remove('open') : currentPlus.classList.add('open');
   
 }
 
-
-
-
 plusButton.forEach(
   plusButton => 
   plusButton.addEventListener('click', handlePlusClick)
   );
+  
+/**
+ * Playing around with sidecart
+ */
+
+  const headerCart = document.querySelector('.elementor-menu-cart__toggle .elementor-button-icon');
+  const dataCounter = headerCart.dataset.counter;
+  const cartContainer = document.querySelector('.elementor-menu-cart__container');
+  
+  let count = localStorage.getItem('cartCount') || 0;
+
+function showSideCart() {
+  
+  if ( dataCounter !== "0" ) {
+    count++
+    localStorage.setItem('cartCount', count);
+  } else if ( dataCounter === "0" ) {
+    count = parseInt(dataCounter);
+    localStorage.setItem('cartCount', count);
+  }
+    
+  if( count === 1) {
+    console.log('sidecart function triggering')
+    cartContainer.classList.add('elementor-menu-cart--shown');
+  }
+    
+  
+}
+
+headerCart.addEventListener('datachange', showSideCart());
